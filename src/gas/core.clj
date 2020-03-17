676ee9e7b23078f0855b3d1e1dec6da451eae41e
Charlie Burrows <charlie.burrows@gmail.com>
2020-03-17 00:37:16 -0400

    wip

--- src/gas/core.clj
+++ src/gas/core.clj
@@ -15,28 +15,53 @@
 
 (defn adjacent-buckets [buckets d]
   (loop [bs (keys buckets)
-         adjs []]
-    (if bs
-      (recur 
-        (rest bs)
-        (if (>= d (- (second bs) (first bs)))
-          (if (and (nth bs 3) (>= d (- (nth bs 3) (second bs))))
-            (cons (take 3 bs) adjs)
-            (cons (take 2 bs) adjs))
-          (if (and (nth bs 3) (>= d (- (nth bs 3) (second bs))))
-            (cons (take 2 (rest bs) adjs))
-            adjs)))
-      adjs)))
-        
+         adjs (if (<= (- (second bs) (first bs)) d)
+                {(first bs) (list (second bs))}
+                {})]
+    (println bs)
+    (let [three (take 3 bs)]
+      (println three)
+      (if (not (= 3 (count (take 3 bs))))
+        adjs
+        (recur 
+          (rest bs)
+          (cond
+            (<= (- (nth three 2) (first three)) (* 2 d))
+            (assoc adjs (second three) (list (first three) (nth three 2)))
 
+            (<= (- (nth three 2) (second three)) d)
+            (assoc adjs (second three) (list (nth three 2)))
 
-(defn collisions [buckets])
+          :otherwise adjs))))))
+
+(defn collisions
+  "Yields a list of pairs of colliding particles"
+  [buckets]
+  (loop [bys (vals buckets)
+         bxs (vals (buckets (first bys)))
+         pairs []]
+    (cond (nil? bys) pairs
+          (nil? bxs) (recur (rest bys) (vals (buckets (second bys))) pairs)
+          :otherwise 
+          (recur bys (rest bxs)
+                 (let [bx (first bxs)
+                       by (first bys)
+                       parts ((buckets by) bx)]
+                   (reduce
+                     (fn [prs part]
+                       (let [d 2
+                             right ((buckets d) (+ b-x d))
+                             down  ((buckets (+ b-y d)) b-x)
+                             dori  ((buckets (+ b-y d)) (+ b-x d))]))
+                     pairs
+                   parts))))))
+
 (defn foo
   "main?"
   []
-  (let [d 2 ; particle diameter
+  let [d 2 ; particle diameter
         ps (mk-particles 20 10 10 1)
         buckets (into {} (map (fn [[k v]] 
                                 [k (bucket-by v :x d)])
                               (bucket-by ps :y d)))]
-    (println (keys buckets))))
+    (println (keys buckets)))
